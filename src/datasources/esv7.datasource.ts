d15e03ab878feb70b87cee7b423360a1917c02e7
Clayton Figueiredo <claytongf@gmail.com>
2021-04-20 08:48:51 -0300

    First Commit

---
+++ src/datasources/esv7.datasource.ts
@@ -0,0 +1,60 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'esv7',
+  connector: 'esv6',
+  index: 'catalog',
+  version: 7,
+  // defaultSize: '',
+  configuration: {
+    node: process.env.ELASTIC_SEARCH_HOST,
+    requestTimeout: process.env.ELASTIC_SEARCH_REQUEST_TIMEOUT,
+    pingTimeout: process.env.ELASTIC_SEARCH_PING_TIMEOUT,
+  },
+  mappingProperties: {
+    docType: {
+      type: 'keyword',
+    },
+    id: {
+      type: 'keyword',
+    },
+    name: {
+      type: 'text',
+      fields: {
+        keyword: {
+          type: 'keyword',
+          ignore_above: 256,
+        },
+      },
+    },
+    is_active: {
+      type: 'boolean',
+    },
+    created_at: {
+      type: 'date',
+    },
+    updated_at: {
+      type: 'date',
+    },
+  }
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class Esv7DataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'esv7';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.esv7', {optional: true})
+    dsConfig: object = config,
+  ) {
+    console.log('config = ', config);
+    super(dsConfig);
+  }
+}
