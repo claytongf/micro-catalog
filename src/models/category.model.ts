337439b91b941ec036038e0d5e60ec839ba7d82d
Clayton Figueiredo <claytongf@gmail.com>
2021-04-27 15:58:20 -0300

    Category Model

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,33 +1,53 @@
+
 import {Entity, model, property} from '@loopback/repository';
 
 @model()
 export class Category extends Entity {
-  @property({
-    type: 'string',
-    id: true,
-    generated: false,
-    required: true,
-  })
-  id: string;
+    @property({
+        type: 'string',
+        id: true,
+        generated: false,
+        required: true,
+    })
+    id: string;
 
-  @property({
-    type: 'string',
-    required: true,
-  })
-  name: string;
+    @property({
+        type: 'string',
+        required: true,
+    })
+    name: string;
 
-  @property({
-    type: 'string',
-  })
-  description?: string;
+    @property({
+        type: 'boolean',
+        require: false,
+    })
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    is_active = true;
 
-  constructor(data?: Partial<Category>) {
-    super(data);
-  }
+    @property({
+        type: 'date',
+        require: true,
+    })
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    created_at: string;
+
+    @property({
+        type: 'date',
+        require: true,
+    })
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    updated_at: string;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [prop: string]: any;
+
+    constructor(data?: Partial<Category>) {
+        super(data);
+    }
 }
 
 export interface CategoryRelations {
-  // describe navigational properties here
+    // describe navigational properties here
 }
 
 export type CategoryWithRelations = Category & CategoryRelations;
